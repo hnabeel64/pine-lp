36cab56d1a0432fd60bb0aefd11cd31a26ea6b68
Nabeel Ali Khan <hnabeel64@gmail.com>
2024-04-22 01:24:27 +0500

    Product Bug Resolved

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
-import { majallab, poppins } from './customfont';
-
+import localFont from 'next/font/local';
+import { Poppins } from 'next/font/google';
+ 
+// Font files can be colocated inside of `pages`
+const majallab = localFont({ 
+    src: './majallab-webfont.woff2',
+    variable: '--font-majallab',
+});
+const poppins = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-poppins',
+    weight: ['400', '500', '600'],
+});
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${poppins.variable, majallab.variable}`}>
